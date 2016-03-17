e9cacfbac8926c272256fe5381c20b52ae8eb5e8
Kris Dolbeare <kris@dolbeare.com>
2016-03-17 10:35:01 -0600

    user & cart changes

---
+++ MetaClothes/MetaClothingJPA/src/entity/Cart.java
@@ -0,0 +1,24 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cart")
+public class Cart
+{	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "userid")
+	private int userId;
+	
+	private int itemId;
+	private int quantity;
+	
+}
