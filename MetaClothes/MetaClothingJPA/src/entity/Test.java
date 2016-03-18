e2d9b56f14f06a2bbe8d10c1ad54f775eadce50d
Shelby Escobedo <shelby.escobedo@gmail.com>
2016-03-18 10:02:29 -0600

    changed csv file

--- MetaClothes/MetaClothingJPA/src/entity/Test.java
+++ MetaClothes/MetaClothingJPA/src/entity/Test.java
@@ -1,8 +1,23 @@
 package entity;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Test {
 	public static void main(String args [])
 	{
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("metaPU");
+		EntityManager em = emf.createEntityManager();
 		
+		em.getTransaction().begin();
+		User user = (User)em.createNamedQuery("user.getUserByEmail").setParameter("email", "indarys@gmail.com").getSingleResult();
+		
+		System.out.println(user);
+		//System.out.println(em.contains(user));
+		
+		
+		
+		em.getTransaction().commit();
 	}
 }
