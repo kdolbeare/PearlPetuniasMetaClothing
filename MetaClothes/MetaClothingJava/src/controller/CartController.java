91f01036b7fec301d074ed7e83f926e16e0b747b
Shelby Escobedo <shelby.escobedo@gmail.com>
2016-03-20 16:22:38 -0600

    everything works now

--- MetaClothes/MetaClothingJava/src/controller/CartController.java
+++ MetaClothes/MetaClothingJava/src/controller/CartController.java
@@ -1,9 +1,11 @@
 package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import dao.CartDAO;
@@ -12,16 +14,17 @@ import entity.Cart;
 import entity.CartItem;
 import entity.User;
 
+@Controller
 public class CartController {
 	@Autowired
 	CartDAO cartDao;
 	@Autowired
 	UserDAO userDao;
 	@ResponseBody
-	@RequestMapping("cart/{id}")
+	@RequestMapping(path="cart/{id}", method=RequestMethod.GET)
 	public Cart getUserCart(@PathVariable("id") int id){
 		User user = userDao.getUserById(id);		
-		return cartDao.getUserCart(user);
+		return cartDao.getCartById(id);
 	}
 	
 	@ResponseBody
