7a45be2b599ab184158e40d704453a17a03dfa0a
Kris Dolbeare <kris@dolbeare.com>
2016-03-21 14:17:18 -0600

    signup in process

---
+++ metaclothingjavascript/public/helpers/signup.js
@@ -0,0 +1,29 @@
+onload = function() {
+    console.log("signup loaded");
+
+    document.getElementById("submit").addEventListener('click', function(e) {
+        e.preventDefault();
+        var isEmployee = false;
+        if (isEmployee.checked){
+          isEmployee = true;
+        }
+        var isBilling = true;
+        if (!isBilling.checked){
+          isBilling = false;
+        }
+        var user = {
+          name: document.signupForm.name.value,
+          email: document.signupForm.email.value,
+          password: document.signupForm.password.value,
+          isEmployee: isEmployee;
+          address: {
+            streetAddress: document.signupForm.streetAddress.value,
+            city: document.signupForm.city.value,
+            stateAbbrev: document.signupForm.stateAbbrev.value,
+            zipcode: document.signupForm.zipcode.value,
+            isBilling: isBilling
+          };
+        };
+      });
+      console.log(user);
+    }
--- metaclothingjavascript/server.js
+++ metaclothingjavascript/server.js
@@ -28,10 +28,10 @@ app.use(express.static(__dirname + '/public'));
 var aboutLib = require('./lib/about.js');
 var loginLib = require('./lib/login.js');
 var storeLib = require('./lib/store.js');
+var signupLib = require('./lib/signup.js');
 
-var credentials = require('./public/credentials.js');
 var cookieParser = require('cookie-parser');
-app.use(cookieParser(credentials().secret));
+app.use(cookieParser(credentials.cookieSecret));
 
 app.get('/setCookie',function(req,res){
   if(!req.signedCookies.cart){
@@ -45,7 +45,7 @@ app.get('/setCookie',function(req,res){
 });
 
 app.post('/addToCart',function(req,res){
-  
+
   var item = req.body.item;
   var price = Number(req.body.price);
   var quant = Number(req.body.quant);
@@ -91,6 +91,11 @@ app.get('/about', function(req,res) {
 app.get('/login', function(req,res) {
 	console.log("login in server.js");
 	res.render('login', {page : loginLib.getLogin()});
+});
+
+app.get('/signup', function(req,res) {
+	console.log("signup in server.js");
+	res.render('signup', {page : signupLib.getSignup()});
 });
 
 //api connections
