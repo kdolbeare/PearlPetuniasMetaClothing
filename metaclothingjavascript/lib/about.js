cd6cece7afcb6172e486619194975b6e039e2163
lidja007 <lidja007@gmail.com>
2016-03-22 17:01:38 -0600

    basic layout on about page complte, picture sizes, employee objects

--- metaclothingjavascript/lib/about.js
+++ metaclothingjavascript/lib/about.js
@@ -9,10 +9,51 @@ var about = {
     zip: "80220"
   },
   employees:[{name:'Cole Frock',
-              bio:'CEO of MetaClothing is Cole Frock'},
+              bio:'A one liner goes here.',
+              fullBio:'A multiple paragraph blurb can go here. It can outline the basic full bio found on websites, about employess and they\'re credentials',
+              position:'CEO',
+              imgPath:'img/cole.jpeg'},
               
               {name:'Kris Kane',
-              bio:'CTO of MetaClothing'}]
+              bio:'',
+              fullBio:'',
+              position:'CTO'},
+
+              {name:'Alexandra Bennett',
+              bio:'', 
+              fullBio:'',
+              position:'',
+              imgPath:'img/Alexandra_Bennett.jpg'},
+
+              {name:'David Silver',
+              bio:"",
+              fullBio:'',
+              position:'',
+              imgPath:'img/David_Silver.jpg'},
+
+              {name:'Erika Miller',
+              bio:'',
+              fullBio:'',
+              position:'',
+              imgPath:'img/Erika_Miller.jpg'},
+
+              {name:'Peter Chestnut',
+              bio:'',
+              fullBio:'',
+              position:'',
+              imgPath:'img/Peter_Chestnut.jpg'},
+
+              {name:'Michael James',
+              bio:'',
+              fullBio:'',
+              position:'',
+              imgPath:'img/Michael_James.jpg'},
+
+              {name:'Ignatius J Reilly',
+              bio:'',
+              fullBio:'',
+              position:'',
+              imgPath:''}]
 };
 
 exports.getAbout = function() {
